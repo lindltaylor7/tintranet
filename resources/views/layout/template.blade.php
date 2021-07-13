f0305af8be22b17142e158d7a0f3be6af73a266c
JulioEduardoFabian <julio.edward.fabian@gmail.com>
2021-07-12 19:33:45 -0500

    Perfil1

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -14,13 +14,15 @@
         
         <!-- Data table CSS -->
         <link href="{{asset('vendors/bower_components/datatables/media/css/jquery.dataTables.min.css')}}" rel="stylesheet" type="text/css"/>
-        <link href="{{asset('vendors/bower_components/jquery-toast-plugin/dist/jquery.toast.min.css')}}" rel="stylesheet" type="text/css">
+     
+   
+        <link href="{{asset('vendors/bower_components/jquery-toast-plugin/dist/jquery.toast.min.css')}}" rel="stylesheet" type="text/css"/>
             
         <!-- vector map CSS -->
 		<link href="{{asset('vendors/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>
 
         <!-- Custom CSS -->
-        <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css"/>
     </head>
 
     <body>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("home");
+
+Route::get('/mi_perfil', function () {
+    return view('usuario_perfil.usuario_perfil');
+})->name("perfil");
+
