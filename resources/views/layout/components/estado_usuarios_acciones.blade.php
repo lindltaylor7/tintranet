cad487404f974141be7879bfc3aa298e12e56945
JulioEduardoFabian <julio.edward.fabian@gmail.com>
2021-08-16 11:18:02 -0500

    actualizandov1

---
+++ resources/views/layout/components/estado_usuarios_acciones.blade.php
@@ -0,0 +1,99 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="panel panel-default card-view">
+            <div class="panel-heading">
+                <div class="pull-left">
+                    <h6 class="panel-title txt-dark">Estado de los Proyectos</h6>
+                </div>
+                <div class="pull-right">
+                    <a href="#" class="pull-left inline-block full-screen mr-15">
+                        <i class="zmdi zmdi-fullscreen"></i>
+                    </a>
+                    <div class="pull-left inline-block dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false" role="button"><i class="zmdi zmdi-more-vert"></i></a>
+                        <ul class="dropdown-menu bullet dropdown-menu-right"  role="menu">
+                            <li role="presentation"><a href="javascript:void(0)" role="menuitem"><i class="icon wb-reply" aria-hidden="true"></i>Update</a></li>
+                            <li role="presentation"><a href="javascript:void(0)" role="menuitem"><i class="icon wb-share" aria-hidden="true"></i>Edit</a></li>
+                            <li role="presentation"><a href="javascript:void(0)" role="menuitem"><i class="icon wb-trash" aria-hidden="true"></i>Remove</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="panel-wrapper collapse in">
+                <div class="panel-body row pa-0">
+                  <div class="col-md-12">
+                    <div class="pt-20">
+                        <div class="streamline user-activity">
+                            <div class="sl-item">
+                                <a href="javascript:void(0)">
+                                    <div class="sl-avatar avatar avatar-sm avatar-circle">
+                                        <img class="img-responsive img-circle" src="{{asset('img/user.png')}}" alt="avatar"/>
+                                    </div>
+                                    <div class="sl-content">
+                                        <p class="inline-block"><span class="capitalize-font txt-primary mr-5 weight-500">Clay Masse</span><span>invited to join the meeting in the conference room at 9.45 am</span></p>
+                                        <span class="block txt-grey font-12 capitalize-font">3 Min</span>
+                                    </div>
+                                </a>
+                            </div>
+            
+                            <div class="sl-item">
+                                <a href="javascript:void(0)">
+                                    <div class="sl-avatar avatar avatar-sm avatar-circle">
+                                        <img class="img-responsive img-circle" src="{{asset('img/user1.png')}}" alt="avatar"/>
+                                    </div>
+                                    <div class="sl-content">
+                                        <p class="inline-block"><span class="capitalize-font txt-primary mr-5 weight-500">Evie Ono</span><span>added three new photos in the library</span></p>
+                                        <div class="activity-thumbnail">
+                                            <img src="{{asset('img/thumb-1.jpg')}}" alt="thumbnail"/>
+                                            <img src="{{asset('img/thumb-2.jpg')}}" alt="thumbnail"/>
+                                            <img src="{{asset('img/thumb-3.jpg')}}" alt="thumbnail"/>
+                                        </div>
+                                        <span class="block txt-grey font-12 capitalize-font">8 Min</span>
+                                    </div>
+                                </a>
+                            </div>
+            
+                            <div class="sl-item">
+                                <a href="javascript:void(0)">
+                                    <div class="sl-avatar avatar avatar-sm avatar-circle">
+                                        <img class="img-responsive img-circle" src="{{asset('img/user2.png')}}" alt="avatar"/>
+                                    </div>
+                                    <div class="sl-content">
+                                        <p class="inline-block"><span class="capitalize-font txt-primary mr-5 weight-500">madalyn rascon</span><span>assigned a new task</span></p>
+                                        <span class="block txt-grey font-12 capitalize-font">28 Min</span>
+                                    </div>
+                                </a>
+                            </div>
+            
+                            <div class="sl-item">
+                                <a href="javascript:void(0)">
+                                    <div class="sl-avatar avatar avatar-sm avatar-circle">
+                                        <img class="img-responsive img-circle" src="{{asset('img/user3.png')}}" alt="avatar"/>
+                                    </div>
+                                    <div class="sl-content">
+                                        <p class="inline-block"><span class="capitalize-font txt-primary mr-5 weight-500">Ezequiel Merideth</span><span>completed project wireframes</span></p>
+                                        <span class="block txt-grey font-12 capitalize-font">yesterday</span>
+                                    </div>
+                                </a>
+                            </div>
+            
+                            <div class="sl-item">
+                                <a href="javascript:void(0)">
+                                    <div class="sl-avatar avatar avatar-sm avatar-circle">
+                                        <img class="img-responsive img-circle" src="{{asset('img/user4.png')}}" alt="avatar"/>
+                                    </div>
+                                    <div class="sl-content">
+                                        <p class="inline-block"><span class="capitalize-font txt-primary mr-5 weight-500">jonnie metoyer</span><span>created a group 'Hencework' in the discussion forum</span></p>
+                                        <span class="block txt-grey font-12 capitalize-font">18 feb</span>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
