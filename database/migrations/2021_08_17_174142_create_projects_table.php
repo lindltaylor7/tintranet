f61953396c8a17fdd3e3e250893e213a15101201
bryanHA95 <bryan.huaricapcha.arias@gmail.com>
2021-08-17 13:20:52 -0500

    Actualizacion migraciones

---
+++ database/migrations/2021_08_17_174142_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('amount',11,2)->nullable();
+            $table->date('start_date');
+            $table->date('final_date');
+            $table->date('delivery_date')->nullable();
+            $table->string('status',25);
+            $table->unsignedBigInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
